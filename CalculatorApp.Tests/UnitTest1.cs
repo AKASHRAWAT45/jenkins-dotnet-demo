b0f785ed7f75833c036707ba3849efc0e95abef4
AKASHRAWAT45 <akash.rawat24.ar@gmail.com>
2026-01-12 04:28:25 +0530

    Add project files.

---
+++ CalculatorApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using CalculatorApp;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            int result = calc.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
